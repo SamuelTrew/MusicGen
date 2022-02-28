68cbf3138bb245d2e5d18279b911378d65d0a27f
Samuel Trew <samuel.trew@outlook.com>
2022-02-28 17:59:59 +0000

    Got chords working nicely

--- app/Main.hs
+++ app/Main.hs
@@ -13,6 +13,10 @@ type Samples = Float
 
 type Pulse = Float
 
+type Beats = Float
+
+type Note = [Pulse]
+
 main :: IO ()
 main = do
   saveWave output
@@ -27,20 +31,38 @@ volume = 0.5
 sampleRate :: Samples
 sampleRate = 48000
 
-duration :: Seconds
-duration = 0.5
+duration :: Beats
+duration = 4.0
 
 output :: FilePath
 output = "output.bin"
+
+bpm :: Beats
+bpm = 120.0
+
+beatDuration :: Seconds
+beatDuration = 60.0 / bpm
+
+attack :: [Pulse]
+attack = map (min 1.0) [0.0, 0.001 ..]
 
 noteToStep :: Hz -> Float
 noteToStep note = (note * 2 * pi) / sampleRate
 
 frequency :: Hz -> Seconds -> Float -> [Pulse]
-frequency note duration volume = map (* volume) $ map sin $ map (* noteToStep note) [0.0 .. sampleRate * duration]
+frequency note duration volume =
+  map (* volume) $ zipWith3 (\x y z -> x * y * z) attack outWave release
+  where
+    outWave = map sin $ map (* noteToStep note) [0.0 .. sampleRate * duration]
 
-note :: Semitones -> Seconds -> Float -> [Pulse]
-note n = frequency (frequencyCalc n)
+    release :: [Pulse]
+    release = reverse $ take (length outWave) attack
+
+note :: Semitones -> Beats -> Float -> [Pulse]
+note n beats = frequency (frequencyCalc n) (beats * beatDuration)
+
+chord :: [Note] -> Note
+chord notes = foldl' (zipWith (+)) (take (round $ sampleRate * duration) (repeat 0.0)) notes
 
 majorScale :: [Pulse]
 majorScale =
@@ -56,7 +78,59 @@ majorScale =
     ]
 
 wave :: [Pulse]
-wave = concat [note (2 * i) duration volume | i <- [0 .. 10]]
+wave =
+  concat
+    [ --
+      note 0 0.25 volume,
+      note 0 0.25 volume,
+      note 0 0.25 volume,
+      note 0 0.25 volume,
+      note 0 0.5 volume,
+      --
+      note 0 0.25 volume,
+      note 0 0.25 volume,
+      note 0 0.25 volume,
+      note 0 0.25 volume,
+      note 0 0.25 volume,
+      note 0 0.25 volume,
+      note 0 0.5 volume,
+      --
+      note 5 0.25 volume,
+      note 5 0.25 volume,
+      note 5 0.25 volume,
+      note 5 0.25 volume,
+      note 5 0.25 volume,
+      note 5 0.25 volume,
+      note 5 0.5 volume,
+      --
+      note 3 0.25 volume,
+      note 3 0.25 volume,
+      note 3 0.25 volume,
+      note 3 0.25 volume,
+      note 3 0.25 volume,
+      note 3 0.25 volume,
+      note 3 0.5 volume,
+      --
+      note (-2) 0.5 volume,
+      --
+      note 0 0.25 volume,
+      note 0 0.25 volume,
+      note 0 0.25 volume,
+      note 0 0.25 volume,
+      note 0 0.5 volume
+    ]
 
 saveWave :: FilePath -> IO ()
-saveWave path = Lazy.writeFile path $ Bob.toLazyByteString $ fold $ map Bob.floatLE wave
+saveWave path =
+  Lazy.writeFile path $
+    Bob.toLazyByteString $
+      fold $
+        map
+          Bob.floatLE
+          ( chord
+              [ note 0 duration volume,
+                note 4 duration volume,
+                note 7 duration volume,
+                note 12 duration volume
+              ]
+          )
